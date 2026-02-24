bf787c38201fdacb4b5dfa37e7b86a2e484c855a
rashid.artoghrul <rashid.artoghrul@gmail.com>
2026-02-24 10:56:53 +0400

    added Person.java

---
+++ Person.java
@@ -0,0 +1,43 @@
+package Lab4;
+
+public class Person {
+  private String firstName, lastName, gender;
+
+  public Person(String firstName, String lastName, String gender) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return this.firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getGender() {
+    return this.gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String toString() {
+    return this.firstName + " " + this.lastName + " (" + gender + ")";
+  }
+
+  public boolean equals(Person p) {
+    return (this.firstName == p.firstName) && (this.lastName == p.lastName) && (this.gender == p.gender);
+  }
+}
